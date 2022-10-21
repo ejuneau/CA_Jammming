8fa4cfd32a9cbdd442a47bfbad43f62b990b2aac
ejuneau <rcjuneau8@gmail.com>
2022-10-21 10:33:15 -0400

    Updated to work on Github

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,7 +4,9 @@ import './SearchBar.css';
 export class SearchBar extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            term: ''
+        }
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
     }
